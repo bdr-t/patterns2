0cc39d1a8cb597274ebce6c8b8fd91c790d75e93
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-07-21 10:00:12 +0200

    Create index.js

---
+++ decorator/index.js
@@ -0,0 +1,25 @@
+const decorator = require('./decorator')
+
+
+let items = [
+    ['USD', 9.99, 'Spotify'],
+    ['GBP', 9.99, 'Spotify'],
+    ['CHF', 12.95, 'Spotify'],
+    ['JPY', 980, 'Spotify'],
+    ['CAD', 9.99, 'Spotify'],
+    ['USD', 9.99, 'Spotify'],
+] 
+
+
+
+function calculatePrice(decorator, currency, price, item){
+    this.convert = decorator()
+    let toEur = this.convert(currency, price)
+    console.log(`El preu de ${item} convertit a EUR desde ${currency} es de ${Math.round(toEur * 100) / 100} el cost anual seria de ${Math.round(toEur * 12 * 100) / 100}`)
+    
+    
+}
+
+for(let x of items){
+    calculatePrice(decorator, x[0], x[1], x[2])
+}
